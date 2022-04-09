622b0b8ce3a7146ebb08fe3b9db67258bec572fb
Logan Koshenka <koshenka27@gmail.com>
2022-04-09 09:11:52 -0400

    finished part 3

--- WeatherApp/Managers/WeatherManager.swift
+++ WeatherApp/Managers/WeatherManager.swift
@@ -12,7 +12,7 @@ import CoreLocation
 
 class WeatherManager {
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=d5c716f8adfece581660d34df5ecc242") else { fatalError("Missing URL") }
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=d5c716f8adfece581660d34df5ecc242&units=imperial") else { fatalError("Missing URL") }
         
         let urlRequest = URLRequest(url: url)
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
--- WeatherApp/Views/WeatherView.swift
+++ WeatherApp/Views/WeatherView.swift
@@ -11,7 +11,84 @@ struct WeatherView: View {
     var weather: ResponseBody
     
     var body: some View {
-        Text("Hello, World!")
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold()
+                        .font(.title)
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "cloud")
+                            
+                            Text(weather.weather[0].main)
+                        }
+                        .frame(width: 100, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(String(weather.main.feelsLike.roundDouble()) + "°")
+                            .font(.system(size: 100, weight: .bold))
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Weather now")
+                        .bold()
+                        .padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min", value: weather.main.tempMin.roundDouble() + "°")
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max", value: weather.main.tempMax.roundDouble() + "°")
+                    }
+                    
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind Speed", value: weather.wind.speed.roundDouble() + " mph")
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: weather.main.humidity.roundDouble() + "%")
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.677, saturation: 1.0, brightness: 0.44))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.677, saturation: 1.0, brightness: 0.44))
+        .preferredColorScheme(.dark)
     }
 }
 
